30d65bd1a7d9c5ef766d6ac392e56f7bc37d76cf
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-09 17:50:55 +0900

    Initial commit

---
+++ src/main/java/org/takensoft/taken_soft/TakenSoftApplication.java
@@ -0,0 +1,13 @@
+package org.takensoft.taken_soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakenSoftApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TakenSoftApplication.class, args);
+    }
+    
+}
