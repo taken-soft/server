10accd08297b007bd69847695cd40528ff2ddc11
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-11 20:35:13 +0900

    [ADD] 센서 데이터 자동 생성 모듈 작성

--- src/main/java/org/takensoft/taken_soft/TakenSoftApplication.java
+++ src/main/java/org/takensoft/taken_soft/TakenSoftApplication.java
@@ -2,8 +2,10 @@ package org.takensoft.taken_soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TakenSoftApplication {
     
     public static void main(String[] args) {
