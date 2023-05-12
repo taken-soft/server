b67e0721f1680f6bff75657dc664c8a36c793d52
Vinca0121 <98202797+Vinca0121@users.noreply.github.com>
2023-05-12 09:40:36 +0900

    Revert "Feature/entitiy setting"

--- src/main/java/org/takensoft/taken_soft/TakenSoftApplication.java
+++ src/main/java/org/takensoft/taken_soft/TakenSoftApplication.java
@@ -2,10 +2,8 @@ package org.takensoft.taken_soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TakenSoftApplication {
     
     public static void main(String[] args) {
