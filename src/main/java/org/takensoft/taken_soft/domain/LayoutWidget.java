4ae7610f07a50d98000da4cbe0da727c283ecdd2
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-15 16:32:22 +0900

    [FIX] Layout Widget Property 타입 jsonb 타입으로 변경

--- src/main/java/org/takensoft/taken_soft/domain/LayoutWidget.java
+++ src/main/java/org/takensoft/taken_soft/domain/LayoutWidget.java
@@ -1,18 +1,25 @@
 package org.takensoft.taken_soft.domain;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.takensoft.taken_soft.dto.LayoutWidgetProperty;
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Set;
-
+import org.hibernate.annotations.TypeDef;
 @Entity
 @Table(name = "layout_widget")
 @Getter
 @Setter
-public class LayoutWidget {
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+public class LayoutWidget implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "layout_widget_id", nullable = false)
@@ -30,8 +37,9 @@ public class LayoutWidget {
     @Column(name = "layout_widget_color", length = 40)
     private String layoutWidgetColor;
     
-    @Column(name = "layout_widget_property", length = Integer.MAX_VALUE)
-    private String layoutWidgetProperty;
+    @Type(type = "jsonb")
+    @Column(name = "layout_widget_property", length = Integer.MAX_VALUE, columnDefinition = "jsonb")
+    private LayoutWidgetProperty layoutWidgetProperty;
     
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "widget_id", nullable = false)
---
+++ src/main/java/org/takensoft/taken_soft/dto/LayoutWidgetProperty.java
@@ -0,0 +1,17 @@
+package org.takensoft.taken_soft.dto;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * package :  org.takensoft.taken_soft.dto
+ * fileName : LayoutWidgetProperty
+ * author :  ShinYeaChan
+ * date : 2023-05-15
+ */
+@Data
+public class LayoutWidgetProperty {
+    Map<String,Object> graphColor=new HashMap<>();
+}
