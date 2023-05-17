41b027d7e1dfff83ebc254f49634364bdd567706
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-17 15:17:43 +0900

    [FIX] Builder 패턴 대신 static factory method 사용을 위한 수정

--- src/main/java/org/takensoft/taken_soft/dto/response/SingleDashboardResponse.java
+++ src/main/java/org/takensoft/taken_soft/dto/response/SingleDashboardResponse.java
@@ -6,13 +6,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.takensoft.taken_soft.domain.Layout;
+import org.takensoft.taken_soft.dto.LayoutDto;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 
-/** 사용자에게 대시보드 1개의 모든 정보를 담아 리턴해주는 DTO */
+
+/**
+ *  대시보드 렌더링 응답 객체
+ *
+ */
 @Data
 @Builder
 @NoArgsConstructor
@@ -22,6 +27,9 @@ public class SingleDashboardResponse {
     private String dashboardTitle;
     private String dashboardType;
     private Integer dashboardSequence;
-    private List<Layout> layoutList=new ArrayList<>();
+    private List<LayoutDto> layoutList=new ArrayList<>();
     /** 수정 필요 - 매우 중요 */
+    public static SingleDashboardResponse create(Integer id,String dashboardTitle,String dashboardType,Integer dashboardSequence,List<LayoutDto> layoutList){
+       return new SingleDashboardResponse(id,dashboardTitle,dashboardType,dashboardSequence,layoutList);
+    }
 }
