95ad888a54832b328f3d8a3f433bdd80333dbdb5
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-25 16:16:57 +0900

    [FIX] 응답 상태 수정

--- src/main/java/org/takensoft/taken_soft/repository/LayoutRepository.java
+++ src/main/java/org/takensoft/taken_soft/repository/LayoutRepository.java
@@ -1,14 +1,11 @@
 package org.takensoft.taken_soft.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.takensoft.taken_soft.domain.Dashboard;
 import org.takensoft.taken_soft.domain.Layout;
 
-@Repository
+@Transactional
 public interface LayoutRepository extends JpaRepository<Layout, Integer> {
-    void deleteByDashboard(Dashboard dashboard);
-    
-
+    void deleteAllByDashboard(Dashboard dashboard);
 }
-
